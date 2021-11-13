e25ed511b50700c8af29f300d175c3b70aeaa5ce
6100m <77129415+6100m@users.noreply.github.com>
2021-11-13 13:39:10 -0600

    Create grab_libs.sh

---
+++ grab_libs.sh
@@ -0,0 +1,13 @@
+mkdir /var/
+mkdir /var/gloom/
+mkdir /var/gloom/key/
+mkdir /var/gloom/key/0000/
+chmod 777 /var/gloom/
+chmod 777 /var/gloom/key
+chmod 777 /var/gloom/key/0000/
+openssl rand -base64 24 >> /var/gloom/key/0000/key.bin
+key_data=$(cat /var/gloom/key/0000/key.bin)
+git clone https://github.com/magical/nlzss/ $PWD/temp_$(cat /var/gloom/key/0000/key.bin)/
+git clone "https://github.com/6100m/wc24-tools-LibraryMod" $PWD/WC24ToolsMod/
+cp -r temp_$(cat /var/gloom/key/0000/key.bin)/ $PWD/WC24ToolsMod/NLZSSLib/
+rm -rf temp_$(cat /var/gloom/key/0000/key.bin)
